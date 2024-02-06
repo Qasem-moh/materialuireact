795b58ff2a13ba24d0e3fa161eb3c0d331048e98
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-07 01:42:57 +0300

    mi

---
+++ src/Components/Create.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Create(props) {
+    return (
+        <div>
+            <h2>create</h2>
+        </div>
+    );
+}
+
+export default Create;
---
+++ src/Components/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tyypography
+function Notes(props) {
+    return (
+        <div>
+            <h3>Notes</h3>
+        </div>
+    );
+}
+
+export default Notes;
